1d390ff232635953f65f5d962433f9d09ad654dc
이지훈 <veryezh@gmail.com>
2019-11-21 20:55:22 +0900

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from time import time
 import os
 
 from Xception import Xception
-from MobileNet import MobileNetv2
+# from MobileNet import MobileNetv2
 # from ResNet import resnet_layer
 # from InceptionResnetV2 import inceptionResNetV2
 
@@ -16,7 +16,7 @@ Theme = 'Animals'
 TRAIN_DIR = os.path.join('./dataset/' + Theme + '/train')
 TEST_DIR = os.path.join('./dataset/' + Theme + '/test')
 
-MODEL_SAVE_FOLDER_PATH = './model/' + Theme + '/'
+MODEL_SAVE_DIR = './model/' + Theme + '/'
 MODEL_SAVE_NAME = Theme + '_Xception.hdf5'
 TENSORBOARD_NAME = Theme + ' ' + MODEL_SAVE_NAME
 
@@ -25,11 +25,12 @@ BATCH_SIZE = 16
 IMAGE_SIZE = (299, 299)
 
 # 학습 데이터 Augmentation
-'''
+
 train_datagen = ImageDataGenerator(rescale=1./255, width_shift_range=0.1, height_shift_range=0.1, rotation_range=90,
                                    zoom_range=0.3, horizontal_flip=True, vertical_flip=True, fill_mode='nearest', validation_split=0.2)
 '''
 train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+'''
 
 # 학습, 테스트 데이터셋의 폴더 별로 자동 라벨링
 train_generator = train_datagen.flow_from_directory(TRAIN_DIR, batch_size=BATCH_SIZE, target_size=IMAGE_SIZE,
@@ -37,29 +38,24 @@ train_generator = train_datagen.flow_from_directory(TRAIN_DIR, batch_size=BATCH_
 validation_generator = train_datagen.flow_from_directory(TRAIN_DIR, batch_size=BATCH_SIZE, target_size=IMAGE_SIZE,
                                                   class_mode='categorical', color_mode='rgb', subset='validation')
 
-# number of classes
 number_of_class = train_generator.num_classes
 
 
-model_file_path = MODEL_SAVE_FOLDER_PATH + 'val_loss={val_loss:.4f} val_acc={val_acc:.4f} ' + MODEL_SAVE_NAME
+model_file_path = MODEL_SAVE_DIR + 'val_loss={val_loss:.4f} val_acc={val_acc:.4f} ' + MODEL_SAVE_NAME
 
-if not os.path.exists(MODEL_SAVE_FOLDER_PATH):
-    os.mkdir(MODEL_SAVE_FOLDER_PATH)
+if not os.path.exists(MODEL_SAVE_DIR):
+    os.mkdir(MODEL_SAVE_DIR)
 
-# 모델 파일이 있을 경우 불러옴
-# if os.listdir(MODEL_SAVE_FOLDER_PATH):
-#     file_list = os.listdir(MODEL_SAVE_FOLDER_PATH)
-#     file_name = file_list[0]
-#     print('load model : ' + file_name)
-#     model = load_model(MODEL_SAVE_FOLDER_PATH + file_name)
-#
+# 학습 이어하기
+# MODEL_FILE_DIR = ''
+# if os.path.exists(MODEL_FILE_DIR):
+#     print('load model : ' + MODEL_FILE_DIR)
+#     model = load_model(MODEL_FILE_DIR)
 # else:
-    # model = MobileNetv2(number_of_class)
-model = Xception(number_of_class)
-    # model = resnet_layer(number_of_class)
-    # model = inceptionResNetV2(number_of_class)
+#     model = Xception(number_of_class)
 
-model.summary()
+model = Xception(number_of_class)
+# model.summary()
 
 # 학습 과정 설정 / Optimizer : Adam, Loss function : Categorical cross-entropy
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -79,7 +75,6 @@ callback_list = [checkpoint, tensorBoard]
 model.fit_generator(train_generator, steps_per_epoch=train_generator.samples // BATCH_SIZE, epochs=EPOCH, callbacks=callback_list,
                     validation_data=validation_generator, validation_steps=validation_generator.samples // BATCH_SIZE)
 
-
 # 모델 저장
-model.save(MODEL_SAVE_FOLDER_PATH + MODEL_SAVE_NAME)
+model.save(MODEL_SAVE_DIR + MODEL_SAVE_NAME)
 print("Saving....")
